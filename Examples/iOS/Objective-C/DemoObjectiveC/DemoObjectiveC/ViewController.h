211de158a7cc54323bb622e854277649ebe207c9
Jonathon Copeland <jcopela4@gmail.com>
2023-10-19 18:17:36 -0400

    Example App for Objective C

---
+++ Examples/iOS/Objective-C/DemoObjectiveC/DemoObjectiveC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  DemoObjectiveC
+//
+//  Created by Jonathon Copeland on 10/19/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Examples/iOS/Objective-C/DemoObjectiveC/DemoObjectiveC/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  DemoObjectiveC
+//
+//  Created by Jonathon Copeland on 10/19/23.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
