fd984c89bad8996f01700ddf655d4db35ee333fd
Ariel Demarco <ariel.demarco@embrace.io>
2023-12-14 17:29:30 -0300

    Added URLSessionTask+Extension tests

---
+++ Tests/EmbraceCoreTests/Capture/Network/URLSessionTask+ExtensionTests.swift
@@ -0,0 +1,46 @@
+//
+//  Copyright © 2023 Embrace Mobile, Inc. All rights reserved.
+//
+    
+
+import XCTest
+@testable import EmbraceCore
+
+class URLSessionTaskExtensionTests: XCTestCase {
+    private var sut: URLSessionTask!
+    private var dataToSet: Data!
+
+    func test_onCreation_embraceDataIsNil() {
+        givenRandomURLSessionTask()
+        thenEmbraceDataIsNil()
+    }
+
+    func test_onSetEmbraceData_dataShouldBeRetained() {
+        givenRandomURLSessionTask()
+        givenRandomDataToSet()
+        whenInvokingSetEmbraceData()
+        thenEmbraceDataShouldHaveThatData()
+    }
+}
+
+private extension URLSessionTaskExtensionTests {
+    func givenRandomURLSessionTask() {
+        sut = URLSession.shared.dataTask(with: URLRequest(url: URL(string: "https://embrace.io")!))
+    }
+
+    func givenRandomDataToSet() {
+        dataToSet = UUID().uuidString.data(using: .utf8)!
+    }
+
+    func whenInvokingSetEmbraceData() {
+        sut.embraceData = dataToSet
+    }
+
+    func thenEmbraceDataIsNil() {
+        XCTAssertNil(sut.embraceData)
+    }
+
+    func thenEmbraceDataShouldHaveThatData() {
+        XCTAssertEqual(dataToSet, sut.embraceData)
+    }
+}
