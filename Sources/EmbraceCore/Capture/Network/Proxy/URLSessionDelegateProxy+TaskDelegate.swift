222834734a8317dbeab28dacc17eeb0791b2caf0
Austin Treat Emmons <austin.emmons@embrace.io>
2024-01-04 17:20:30 -0500

    Add GH Action to use new bin/build script on all platforms (#129)
    * Replaces `bin/build`
    
    Adds EmbraceIO-Package.xcscheme file to be able to prevent test targets from being build when not necessary
    
    * Adds build-platforms github workflow
    
    * Sets fail-fast to false
    
    * Adds xcode_version to build matrix
    
    * Updates job name based on matrix values
    
    * Pipes output through xcpretty
    
    * Adds cache step to job
    
    * Adds `set -e` to bin scripts
    
    * Updates build-platforms step to forward PIPESTATUS
    
    * Updates destination to use `generic/` prefix to platform
    
    * Adds @available checks for tvOS in URLSessionDelegateProxy+TaskDelegate

--- Sources/EmbraceCore/Capture/Network/Proxy/URLSessionDelegateProxy+TaskDelegate.swift
+++ Sources/EmbraceCore/Capture/Network/Proxy/URLSessionDelegateProxy+TaskDelegate.swift
@@ -17,7 +17,7 @@ extension URLSessionDelegateProxy: URLSessionTaskDelegate {
         }
     }
 
-    @available(iOS 16.0, *)
+    @available(iOS 16.0, macOS 13.0, tvOS 16.0, *)
     func urlSession(_ session: URLSession, didCreateTask task: URLSessionTask) {
         let selector = #selector(
             URLSessionTaskDelegate.urlSession(_:didCreateTask:)
@@ -57,8 +57,7 @@ extension URLSessionDelegateProxy: URLSessionTaskDelegate {
         }
     }
 
-    @available(iOS 17.0, *)
-    @available(macOS 14.0, *)
+    @available(iOS 17.0, macOS 14.0, tvOS 17.0, *)
     func urlSession(_ session: URLSession,
                     task: URLSessionTask,
                     didReceiveInformationalResponse response: HTTPURLResponse) {
