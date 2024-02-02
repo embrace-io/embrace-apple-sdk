a7e5dfd190cc07b9b6d9ec056e63b1b0a8b46e6f
Ariel Demarco <ariel.demarco@embrace.io>
2024-02-02 09:17:19 -0300

    Finish Array Extension for EmbraceLogRecordProcessor

--- Tests/EmbraceOTelTests/Logs/BatchLimitTests.swift
+++ Tests/EmbraceOTelTests/Logs/BatchLimitTests.swift
@@ -28,4 +28,3 @@ class BatchLimitsTests: XCTestCase {
         XCTAssertEqual(limits.maxLogsPerBatch, randomNumber)
     }
 }
-
---
+++ Tests/EmbraceOTelTests/Logs/Processors/EmbraceLogRecordProcessor+ArrayExtension.swift
@@ -0,0 +1,18 @@
+//
+//  Copyright © 2023 Embrace Mobile, Inc. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+import OpenTelemetrySdk
+
+@testable import EmbraceOTel
+
+class EmbraceLogRecordProcessorArrayExtensionTests: XCTest {
+    func test_onDefaultWithExporters_returnSingleLogRecordProcessorInstance() throws {
+        let processors: [LogRecordProcessor] = .default(withExporters: [])
+        XCTAssertEqual(processors.count, 1)
+        XCTAssertTrue(try XCTUnwrap(processors.first) is SingleLogRecordProcessor)
+    }
+}
