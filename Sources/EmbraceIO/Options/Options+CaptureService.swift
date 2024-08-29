135fd656d9637cb90f4cf1306407dd63a3924987
Austin Treat Emmons <austin.emmons@embrace.io>
2024-08-29 15:38:43 -0400

    Update Sources/EmbraceIO/Options/Options+CaptureService.swift
    Co-authored-by: ArielDemarco <ariel.demarco@embrace.io>

--- Sources/EmbraceIO/Options/Options+CaptureService.swift
+++ Sources/EmbraceIO/Options/Options+CaptureService.swift
@@ -68,7 +68,8 @@ public extension Embrace.Options {
         )
     }
 
-    /// Initializer for `Embrace.Options` that does not require an appId but used the
+    /// Initializer for `Embrace.Options` that does not require an appId.
+    
     /// Use this initializer if you don't want the SDK to send data to Embrace's servers.
     /// You must provide your own `OpenTelemetryExport`
     ///
