ed2c6fc357d78775823e6385ec0735d979cbe702
Austin Emmons <austin.emmons@embrace.io>
2024-10-03 16:43:05 -0400

    Updates EmbraceStorageBenchmarks to measure migrations

--- Benchmarks/EmbraceStorageBenchmarks/EmbraceStorageBenchmarks.swift
+++ Benchmarks/EmbraceStorageBenchmarks/EmbraceStorageBenchmarks.swift
@@ -8,38 +8,53 @@ import EmbraceStorageInternal
 import EmbraceCommonInternal
 import OpenTelemetryApi
 
-func removeTempDirectory() throws {
-    try FileManager.default.removeItem(at: FileManager.default.temporaryDirectory.appending(path: #file))
-}
-
 let benchmarks = {
-    Benchmark("Insert SpanRecords",
-              configuration: .init(metrics: .all, teardown: removeTempDirectory)
-    ) { benchmark in
-        let tmpURL = FileManager.default.temporaryDirectory
+    Benchmark("Perform Migrations", configuration: .init(metrics: .all)) { benchmark in
+        let tmpURL = FileManager.default.temporaryDirectory.appending(path: #file)
 
-        let storage = try! EmbraceStorage(
-            options: .init(baseUrl: tmpURL.appending(path: #file),
-                           fileName: "benchmarks.sqlite"),
-            logger: .noop
-        )
-        try! storage.performMigration()
-
-        let data = "This is some data".data(using: .utf8)!
-        let date = Date()
         for i in benchmark.scaledIterations {
-            try storage.upsertSpan(
-                SpanRecord(
-                    id: SpanId.random().hexString,
-                    name: "benchmark - \(benchmark.name) #\(i)",
-                    traceId: TraceId.random().hexString,
-                    type: .performance,
-                    data: data,
-                    startTime: date
-                )
+            var storage: EmbraceStorage? = try? EmbraceStorage(
+                options: .init(baseUrl: tmpURL,
+                               fileName: "storage_migrations.sqlite"),
+                logger: .noop
             )
-        }
+            try? storage?.performMigration()
 
-        try FileManager.default.removeItem(at: FileManager.default.temporaryDirectory.appending(path: #file))
+            storage = nil
+
+            try FileManager.default.removeItem(at: tmpURL)
+        }
+    }
+
+    Benchmark("Insert SpanRecord") { benchmark in
+        let tmpURL = FileManager.default.temporaryDirectory.appending(path: #file)
+
+        do {
+            let storage = try EmbraceStorage(
+                options: .init(baseUrl: tmpURL,
+                               fileName: "span_insert.sqlite"),
+                logger: .noop
+            )
+            try storage.performMigration()
+
+            let data = "This is some data".data(using: .utf8)!
+            let date = Date()
+            for i in benchmark.scaledIterations {
+                try storage.upsertSpan(
+                    SpanRecord(
+                        id: SpanId.random().hexString,
+                        name: "benchmark - \(benchmark.name) #\(i)",
+                        traceId: TraceId.random().hexString,
+                        type: .performance,
+                        data: data,
+                        startTime: date
+                    )
+                )
+            }
+
+            try FileManager.default.removeItem(at: tmpURL)
+        } catch let e {
+            throw e
+        }
     }
 }
