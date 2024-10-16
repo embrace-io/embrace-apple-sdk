f8b80ace28733836216119129ad7b653f1806766
Ignacio Tischelman <114942102+NachoEmbrace@users.noreply.github.com>
2024-10-16 12:03:07 -0300

    Updating Dangerfile  (#95)

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -20,15 +20,20 @@ func main() {
     let sourceChanges = editedFiles.first(where: { $0.hasPrefix("Sources") })
     let testChanges = editedFiles.first(where: { $0.hasPrefix("Tests") })
 
-    // check changelog entry only if there were changes in the code
-    if !changelogChanged && sourceChanges != nil {
+    // for release candidates, there must be a changelog update
+    if danger.github.pullRequest.head.label.contains("release/") && !changelogChanged {
+        fail("Releases must have CHANGELOG updates.")
+
+    // otherwise only throw a warning if there were code changes without a changelog update
+    } else if !changelogChanged && sourceChanges != nil {
         warn("No CHANGELOG entry added.")
     }
-
+    
     // check tests only if there were changes in the code
     if sourceChanges != nil && testChanges == nil {
         warn("No tests added / modified.")
     }
+
     // lint modified files
     SwiftLint.lint(.files(editedFiles), inline: true, quiet: false)
 }
