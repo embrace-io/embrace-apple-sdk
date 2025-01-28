3557e99738f5a83911e4a5ea12c9fe5204fc2713
Fernando Draghi <>
2025-01-28 11:59:50 -0300

    refactored ReportCardView

--- Examples/EmbraceIOTestApp/EmbraceIOTestApp/Screens/TestReportCard/TestReportCard.swift
+++ Examples/EmbraceIOTestApp/EmbraceIOTestApp/Screens/TestReportCard/TestReportCard.swift
@@ -6,52 +6,18 @@
 
 import SwiftUI
 
-
-
 struct TestReportCard: View {
     @Environment(\.dismiss) private var dismiss
     @Binding var report: TestReport
     var body: some View {
         VStack {
-            Text(report.passed ? "PASS" : "FAIL")
-                .font(.embraceFont(size: 35))
-                .foregroundStyle(report.passed ? .green : .red)
-                .padding(.bottom, 60)
+            TestReportCardHeaderView(passed: report.passed)
                 .padding(.top, 20)
-            VStack{
-                HStack {
-                    Text("Target")
-                        .font(.embraceFont(size: 12))
-                        .frame(width: 100, alignment: .leading)
-                    Text("Expected")
-                        .font(.embraceFont(size: 12))
-                        .frame(width: 100, alignment: .leading)
-                    Text("Recorded")
-                        .font(.embraceFont(size: 12))
-                        .frame(width: 100, alignment: .leading)
-                    Spacer()
-                }
-            }
-            .padding(.leading, 5)
+                .padding(.bottom, 60)
+            TestReportCardSectionsView()
+                .padding(.leading, 5)
             List(report.items) { item in
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text(item.target)
-                            .font(.embraceFont(size: 11))
-                            .frame(width: 100, alignment: .leading)
-                        Text(item.expected)
-                            .font(.embraceFont(size: 11))
-                            .frame(width: 100, alignment: .leading)
-                        Text(item.recorded)
-                            .font(.embraceFont(size: 11))
-                            .frame(width: 100, alignment: .leading)
-                        Spacer()
-                        Image(systemName: item.passed ? "checkmark" : "xmark")
-                            .foregroundStyle(item.passed ? .green : .red)
-                            .frame(width: 20, alignment: .trailing)
-                            .padding(.trailing, 5)
-                    }
-                }
+                TestReportCardItemView(item: item)
                 .listRowInsets(EdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 0))
             }
             .contentMargins(0)
