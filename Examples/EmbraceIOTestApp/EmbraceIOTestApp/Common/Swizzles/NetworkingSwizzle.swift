f607c1ca4c8fdfe3dce33a9e52125b94dc9606f5
Fernando Draghi <>
2025-05-09 17:12:56 -0300

    WIP: checking exported spans made it to the payload

--- Examples/EmbraceIOTestApp/EmbraceIOTestApp/Common/Swizzles/NetworkingSwizzle.swift
+++ Examples/EmbraceIOTestApp/EmbraceIOTestApp/Common/Swizzles/NetworkingSwizzle.swift
@@ -9,6 +9,8 @@ import OpenTelemetrySdk
 import EmbraceCommonInternal
 import EmbraceCore
 
+typealias JsonDictionary = Dictionary<String, Any>
+
 class NetworkingSwizzle: NSObject {
     private typealias URLSessionCompletion = (Data?, URLResponse?, Error?) -> Void
     private typealias ImplementationType = @convention(c) (URLSession, Selector, URLRequest, URLSessionCompletion?) -> URLSessionDataTask
@@ -19,9 +21,14 @@ class NetworkingSwizzle: NSObject {
 
     static private var initialized = false
 
-    private(set) var capturedJsons: [String: [NSDictionary]] = [:]
-    private var exportedSpansBySession: [String: [SpanData]] = [:]
-    private var exportedLogsBySessions: [String: [ReadableLogRecord]] = [:]
+    /// Contains all Jsons posted, separated by Session Id
+    private(set) var postedJsons: Dictionary<String, Array<JsonDictionary>> = [:]
+
+    /// Contains all exported spans grouped by the Session they were exported on. Including the session span. The Key is the Session Id.
+    private(set) var exportedSpansBySession: Dictionary<String, [SpanData]> = [:]
+
+    /// Contains all the logs exported, grouped by Session Id.
+    private(set) var exportedLogsBySessions: [String: [ReadableLogRecord]] = [:]
 
     init(spanExporter: TestSpanExporter, logExporter: TestLogRecordExporter) {
         super.init()
@@ -46,7 +53,7 @@ class NetworkingSwizzle: NSObject {
             let originalMethod = unsafeBitCast(originalImp, to: ImplementationType.self)
             if let data = urlRequest.httpBody {
                 if let uncompressed = try? data.gunzipped() {
-                    if let json = try? JSONSerialization.jsonObject(with: uncompressed) as? NSDictionary {
+                    if let json = try? JSONSerialization.jsonObject(with: uncompressed) as? Dictionary<String, Any> {
                         self.capturedNewJson(json)
                     }
                 }
@@ -74,20 +81,33 @@ class NetworkingSwizzle: NSObject {
         }
     }
 
-    private func capturedNewJson(_ json: NSDictionary) {
-        guard let currentSessionId = Embrace.client?.currentSessionId() else {
+    private func capturedNewJson(_ json: Dictionary<String, Any>) {
+        let data = json["data"] as? Dictionary<String, Any> ?? [:]
+        let spans = data["spans"] as? Array<Dictionary<String, Any>> ?? []
+        let sessionSpan = spans.first { $0["name"] as? String == "emb-session" }
+        let attributes = sessionSpan?["attributes"] as? Array<Dictionary<String, String>>
+        let sessionIdAttribute = attributes?.first { $0["key"] == "session.id" }
+        let sessionId = sessionIdAttribute?["value"] as? String
+
+        guard let sessionId = sessionId else {
             return
         }
 
-        if self.capturedJsons[currentSessionId] == nil {
-            self.capturedJsons[currentSessionId] = [json]
-        } else {
-            self.capturedJsons[currentSessionId]?.append(json)
-        }
+        self.postedJsons[sessionId, default: []].append(json)
+
+        UploadedSessionPayloadTest().test(networkSwizzle: self)
     }
 
     private func capturedExportedSpan(_ spanExporter: TestSpanExporter) {
-        guard let currentSessionId = Embrace.client?.currentSessionId() else {
+        var currentSessionId: String? = Embrace.client?.currentSessionId()
+        if currentSessionId == nil {
+            let sessionSpan = spanExporter.latestExporterSpans.first { span in
+                span.attributes["emb.type"]?.description == "ux.session"
+            }
+            currentSessionId = sessionSpan?.attributes["session.id"]?.description
+        }
+
+        guard let currentSessionId = currentSessionId else {
             return
         }
 
@@ -108,5 +128,51 @@ class NetworkingSwizzle: NSObject {
         } else {
             exportedLogsBySessions[currentSessionId]?.append(contentsOf: logExporter.latestExportedLogs)
         }
+    }
+}
+
+class UploadedSessionPayloadTest: NSObject {
+    func test(networkSwizzle: NetworkingSwizzle) {
+        let exportedSpansSessionIds = networkSwizzle.exportedSpansBySession.keys
+        guard exportedSpansSessionIds.count > 0 else {
+            return
+        }
+
+        let postedSessionIds = networkSwizzle.postedJsons.keys
+
+        postedSessionIds.forEach { sessionId in
+            print(sessionId)
+            let exportedSpans = networkSwizzle.exportedSpansBySession[sessionId]
+            exportedSpans?.forEach { exportedSpan in
+                let postedJsons = networkSwizzle.postedJsons[sessionId]
+                var foundSpans = 0
+                var missingSpans = 0
+                postedJsons?.forEach { postedJson in
+                    let data = postedJson["data"] as? JsonDictionary
+                    let postedSpans = data?["spans"] as? Array<JsonDictionary>
+                    let span = postedSpans?.first { $0["span_id"] as? String == exportedSpan.spanId.hexString }
+                    if span != nil {
+                        //print("found span: \(exportedSpan.spanId.hexString)")
+                        foundSpans += 1
+                    } else {
+                        print("MISSING span: \(exportedSpan.spanId.hexString)")
+                        print("-- Span Name: \(exportedSpan.name)")
+                        print("-- Span type: \(exportedSpan.embType)")
+                        missingSpans += 1
+                    }
+                }
+            }
+        }
+
+        // Making sure all exported spans were posted
+        let allFound = Set(exportedSpansSessionIds).isSubset(of: postedSessionIds)
+
+        if allFound {
+            print("All sessions were posted")
+        } else {
+            print("some sessions were not posted")
+        }
+
+
     }
 }
