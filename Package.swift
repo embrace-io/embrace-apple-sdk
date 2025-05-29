ad4e9593b7e6fac080030b4f5bb8075a32cae163
Alexander Cohen <naftaly@users.noreply.github.com>
2025-05-29 12:13:49 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -40,10 +40,13 @@ let package = Package(
         .library(name: "EmbraceCrashlyticsSupport", targets: ["EmbraceCrashlyticsSupport"])
     ],
     dependencies: [
+        .package(path: "../KSCrash"),
+        /*
         .package(
             url: "https://github.com/kstenerud/KSCrash",
             exact: "2.3.0"
         ),
+         */
         .package(
             url: "https://github.com/open-telemetry/opentelemetry-swift",
             exact: "2.0.0"
@@ -91,7 +94,8 @@ let package = Package(
                 "EmbraceObjCUtilsInternal",
                 "EmbraceSemantics",
                 "EmbraceBugsnagTools",
-                .product(name: "DemangleFilter", package: "KSCrash")
+                .product(name: "DemangleFilter", package: "KSCrash"),
+                .product(name: "Backtrace", package: "KSCrash")
             ],
             resources: [
                 .copy("PrivacyInfo.xcprivacy")
