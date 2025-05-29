fa935359e9d3edf24cf7915e1fac702cb90a95ad
Alexander Cohen <naftaly@users.noreply.github.com>
2025-05-29 12:13:49 -0400

    wip

--- Sources/EmbraceCore/Backtrace/EmbraceBacktrace.swift
+++ Sources/EmbraceCore/Backtrace/EmbraceBacktrace.swift
@@ -6,54 +6,56 @@ import Foundation
 
 public struct EmbraceBacktraceFrame: Codable {
     public let address: UInt64
-    public let symbolAddress: UInt64
-    public let symbolName: String
-    public let imageUUID: String
-    public let imageName: String
-    public let imageSize: UInt64
-    public let imageOffset: UInt64
+    
+    public struct Symbol: Codable {
+        public let address: UInt64
+        public let name: String
+    }
+    public let symbol: Symbol?
+    
+    public struct Image: Codable {
+        public let uuid: String
+        public let name: String
+        public let size: UInt64
+        public let offset: UInt64
+    }
+    public let image: Image?
 }
 extension EmbraceBacktraceFrame: Sendable {}
+extension EmbraceBacktraceFrame.Symbol: Sendable {}
+extension EmbraceBacktraceFrame.Image: Sendable {}
 
 public struct EmbraceBacktraceThread: Codable {
     public let index: Int
-    public let name: String
-    public let frames: [EmbraceBacktraceFrame]
+    public var frames: [EmbraceBacktraceFrame] {
+        callstack.frames(symbolicated: true)
+    }
+    
+    internal struct Callstack: Codable {
+        let addresses: [UInt]
+        let count: Int
+    }
+    internal let callstack: Callstack
 }
 extension EmbraceBacktraceThread: Sendable {}
+extension EmbraceBacktraceThread.Callstack: Sendable {}
+
+public enum EmbraceBacktraceTimestampUnits: String, Codable {
+    case nanoseconds
+    case milliseconds
+}
+extension EmbraceBacktraceTimestampUnits: Sendable {}
 
 public struct EmbraceBacktrace: Codable {
-    public let timestampUnits: String = "nanoseconds"
+    public let timestampUnits: EmbraceBacktraceTimestampUnits = .nanoseconds
     public let timestamp: UInt64
-    public let symbolicated: Bool
     public let threads: [EmbraceBacktraceThread]
 
     /// Call this to take a stacktrace of the passed in thread.
     static func backtrace(of thread: pthread_t) -> EmbraceBacktrace {
         EmbraceBacktrace(
             timestamp: clock_gettime_nsec_np(CLOCK_MONOTONIC_RAW),
-            symbolicated: false,
             threads: takeSnapshot(of: thread)
-        )
-    }
-    
-    func symbolicate() -> EmbraceBacktrace {
-        guard symbolicated == false else {
-            return self
-        }
-        
-        return EmbraceBacktrace(
-            timestamp: timestamp,
-            symbolicated: true,
-            threads: threads.map { thread in
-                EmbraceBacktraceThread(
-                    index: thread.index,
-                    name: thread.name,
-                    frames: thread.frames.map { frame in
-                        frame.symbolicated()
-                    }
-                )
-            }
         )
     }
 }
