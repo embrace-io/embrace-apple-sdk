8b51230e6aeaaf17e181a8268049efa5f55a14fd
Alexander Cohen <naftaly@users.noreply.github.com>
2025-06-30 17:33:56 -0400

    setting up structure

---
+++ Sources/EmbraceCore/SwiftUI/Internal/EmbraceSurfaceEnvironment.swift
@@ -0,0 +1,26 @@
+//
+//  EmbraceSurfaceEnvironment.swift
+//  Copyright © 2025 Embrace Mobile, Inc. All rights reserved.
+//
+import SwiftUI
+
+struct EmbraceSurfaceViewFramePreferenceKey: PreferenceKey {
+    static var defaultValue: [UUID: CGRect] = [:]
+    
+    static func reduce(value: inout [UUID: CGRect], nextValue: () -> [UUID: CGRect]) {
+        value.merge(nextValue(), uniquingKeysWith: { $1 })
+    }
+}
+
+@available(iOS 13, macOS 10.15, tvOS 13, watchOS 6.0, *)
+private struct EmbraceSurfaceParentEnvironmentKey: EnvironmentKey {
+    static let defaultValue: UUID? = nil
+}
+
+@available(iOS 13, macOS 10.15, tvOS 13, watchOS 6.0, *)
+extension EnvironmentValues {
+    var embraceSurfaceParent: UUID? {
+        get { self[EmbraceSurfaceParentEnvironmentKey.self] }
+        set { self[EmbraceSurfaceParentEnvironmentKey.self] = newValue }
+    }
+}
