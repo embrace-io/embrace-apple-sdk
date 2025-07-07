03369e333033a3438322cad34615c66f50a9e699
Ignacio Tischelman <ignacio.tischelman@embrace.io>
2025-07-07 18:09:02 -0300

    Fixing clean exit not being updated before sending sessions

---
+++ Sources/EmbraceCore/Session/Upload/DefaultSessionUploader.swift
@@ -0,0 +1,21 @@
+//
+//  Copyright © 2025 Embrace Mobile, Inc. All rights reserved.
+//
+
+import Foundation
+#if !EMBRACE_COCOAPOD_BUILDING_SDK
+import EmbraceCommonInternal
+import EmbraceStorageInternal
+import EmbraceUploadInternal
+#endif
+
+// used for tests
+protocol SessionUploader {
+    func uploadSession(_ session: EmbraceSession, storage: EmbraceStorage, upload: EmbraceUpload)
+}
+
+class DefaultSessionUploader: SessionUploader {
+    func uploadSession(_ session: EmbraceSession, storage: EmbraceStorage, upload: EmbraceUpload) {
+        UnsentDataHandler.sendSession(session, storage: storage, upload: upload)
+    }
+}
