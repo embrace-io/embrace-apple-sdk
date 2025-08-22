eefbaa5adcf19b741f60ae29df8afad826351cf2
Alexander Cohen <naftaly@users.noreply.github.com>
2025-08-22 16:51:26 -0400

    clock updates

--- Sources/EmbraceCore/Capture/Hang/HangCaptureService.swift
+++ Sources/EmbraceCore/Capture/Hang/HangCaptureService.swift
@@ -110,7 +110,7 @@ extension HangCaptureService: HangObserver {
         guard
             let builder = buildSpan(
                 name: "emb-thread-blockage",
-                type: SpanType(primary: .performance, secondary: "thread_blockage"),
+                type: .ux, // perf.thread_blockage
                 attributes: [:]
             )
         else {
