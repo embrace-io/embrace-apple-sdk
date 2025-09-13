218ff89a3e411e7cc829b2a69c2ae646cccb8126
Alexander Cohen <naftaly@users.noreply.github.com>
2025-09-13 16:35:02 -0400

    Update CrashReporter.swift

--- Sources/EmbraceCore/Capture/Hang/HangCaptureService.swift
+++ Sources/EmbraceCore/Capture/Hang/HangCaptureService.swift
@@ -69,6 +69,16 @@ public final class HangCaptureService: CaptureService {
             limitData.withLock { $0.limits = newValue }
         }
     }
+
+    private var crashReporterData: EmbraceMutex<EmbraceCrashReporter?> = EmbraceMutex(nil)
+    internal var crashReporter: EmbraceCrashReporter? {
+        get {
+            crashReporterData.withLock { $0 }
+        }
+        set {
+            crashReporterData.withLock { $0 = newValue }
+        }
+    }
 }
 
 extension HangCaptureService: HangObserver {
@@ -79,6 +89,10 @@ extension HangCaptureService: HangObserver {
     public func hangStarted(at: NanosecondClock, duration: NanosecondClock) {
 
         logger?.debug("[Watchdog] Hang started, at \(at.date) after waiting \(duration.uptime.milliseconds) ms")
+
+        crashReporter?.watchdogEventStarted(
+            WatchdogEvent(timestamp: at, duration: duration)
+        )
 
         // Keep tabs on how many hang spans we've created
         let sampleInfo = limitData.withLock {
@@ -128,6 +142,10 @@ extension HangCaptureService: HangObserver {
     public func hangUpdated(at: NanosecondClock, duration: NanosecondClock) {
         logger?.debug("[Watchdog] Hang for \(duration.uptime.milliseconds) ms")
 
+        crashReporter?.watchdogEventOngoing(
+            WatchdogEvent(timestamp: at, duration: duration)
+        )
+
         guard
             limitData.withLock({
                 $0.samplesInHangCount += 1
@@ -151,6 +169,10 @@ extension HangCaptureService: HangObserver {
     public func hangEnded(at: NanosecondClock, duration: NanosecondClock) {
         logger?.debug("[Watchdog] Hang ended at \(at.date) after \(duration.uptime.milliseconds) ms")
 
+        crashReporter?.watchdogEventEnded(
+            WatchdogEvent(timestamp: at, duration: duration)
+        )
+        
         spanQueue.async { [self] in
             span?.end(time: at.date)
             span = nil
@@ -161,6 +183,7 @@ extension HangCaptureService: HangObserver {
 
         dispatchPrecondition(condition: .onQueue(spanQueue))
 
+        // Are we over the limit or don't have a span for some reason?
         guard let span else {
             return
         }
