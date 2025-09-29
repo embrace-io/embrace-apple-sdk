e3b52e70a79ad695af4852c2cefa0e68f1df94d0
Alexander Cohen <naftaly@users.noreply.github.com>
2025-09-29 13:30:08 -0400

    update

--- Sources/EmbraceIO/NewAPI/EmbraceIO.swift
+++ Sources/EmbraceIO/NewAPI/EmbraceIO.swift
@@ -11,22 +11,39 @@ final public class EmbraceIO: Sendable {
 
     public let isAvailable: Bool
 
-    public func start() throws {
-        try Embrace.client?.start()
+    @discardableResult
+    public func start() -> Bool {
+        do {
+            try Embrace.client?.start()
+            return true
+        } catch {
+            failure("init error: \(error)")
+            return false
+        }
     }
 
     nonisolated(unsafe) private let _client: Embrace?
     private init() {
-        self._client = try? Embrace.setup(
-            options: Embrace.Options(
-                appId: Self.appID
+        do {
+            self._client = try Embrace.setup(
+                options: Embrace.Options(
+                    appId: Self.appID
+                )
             )
-        )
-        self.isAvailable = self._client != nil
+            self.isAvailable = self._client != nil
+        } catch {
+            self._client = nil
+            self.isAvailable = false
+            failure("init error: \(error)")
+        }
     }
 }
 
 extension EmbraceIO {
+
+    private func failure(_ msg: String) {
+        print("[EmbraceIO] \(msg)")
+    }
 
     private static let appID: String = {
         Bundle.main.infoDictionary?["EMBApplicationId"] as? String ?? ""
@@ -34,7 +51,7 @@ extension EmbraceIO {
 
     private func expectClient() -> Embrace? {
         guard let client = _client else {
-            print("[EmbraceIO] client is nil!")
+            failure("client is nil!")
             return nil
         }
         return client
@@ -47,7 +64,7 @@ extension EmbraceIO {
 
     public func log(
         _ message: String,
-        attributes: @autoclosure () -> [AttributeKey: AttributeValue]
+        attributes: @autoclosure () -> [AttributeKey: AttributeValueType]
     ) {
         guard let client = expectClient() else {
             return
