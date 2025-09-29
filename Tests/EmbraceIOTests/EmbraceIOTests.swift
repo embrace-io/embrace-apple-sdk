513747a475f06e434cc5a31a9e9f39fa7227667b
Alexander Cohen <naftaly@users.noreply.github.com>
2025-09-29 13:30:08 -0400

    update

--- Tests/EmbraceIOTests/EmbraceIOTests.swift
+++ Tests/EmbraceIOTests/EmbraceIOTests.swift
@@ -8,37 +8,62 @@ import XCTest
 
 class EmbraceIOTests: XCTestCase {
 
-    func test_newAttribtues() {
+    func test_attributesStringConversion() {
 
-        let atts: [EmbraceIO.AttributeKey: EmbraceIO.AttributeValue] = [
+        let atts: [EmbraceIO.AttributeKey: EmbraceIO.AttributeValueType] = [
             "key1": true,
-            "key2": 12,
-            "key3": 12.123,
-            "key4": "string"
+            "key2": "string",
+
+            "key3": Int(-12),
+            "key4": Int8(-12),
+            "key5": Int16(-12),
+            "key6": Int32(-12),
+            "key7": Int64(-12),
+
+            "key8": UInt(12),
+            "key9": UInt8(12),
+            "key10": UInt16(12),
+            "key11": UInt32(12),
+            "key12": UInt64(12),
+
+            "key13": Float(12.123),
+            "key14": Float32(12.123),
+            "key15": Float64(12.123),
+
+            "key16": Double(12.123)
         ]
 
-        let data = try! JSONEncoder().encode(atts)
-        let decoded = try! JSONDecoder().decode([EmbraceIO.AttributeKey: EmbraceIO.AttributeValue].self, from: data)
-        XCTAssertEqual(atts, decoded)
-    }
-
-    func test_internals() {
-
-        let atts: [EmbraceIO.AttributeKey: EmbraceIO.AttributeValue] = [
-            "key1": true,
-            "key2": 12,
-            "key3": 12.123,
-            "key4": "string"
-        ]
-
-        let internalsValue = [
+        let internalsValue: [String: String] = [
             "key1": "true",
-            "key2": "12",
-            "key3": "12.123",
-            "key4": "string"
+            "key2": "string",
+
+            "key3": "-12",
+            "key4": "-12",
+            "key5": "-12",
+            "key6": "-12",
+            "key7": "-12",
+
+            "key8": "12",
+            "key9": "12",
+            "key10": "12",
+            "key11": "12",
+            "key12": "12",
+
+            "key13": "12.123",
+            "key14": "12.123",
+            "key15": "12.123",
+
+            "key16": "12.123"
         ]
 
         let internals = atts.asInternalAttributes()
         XCTAssertEqual(internals, internalsValue)
+    }
+
+    func test_dissalowedAttribute() {
+
+        struct NotAllowed: EmbraceIO.AttributeValueType {}
+
+        XCTAssertFalse(isSupportedAttributeValueType(NotAllowed()))
     }
 }
