33ef6d0671900e31be101b4e3b9bd42e9dfd86e6
Alex Cohen <naftaly@me.com>
2025-10-21 17:15:23 -0400

    Update SessionController.swift

--- Sources/EmbraceCore/Session/SessionController.swift
+++ Sources/EmbraceCore/Session/SessionController.swift
@@ -28,12 +28,6 @@ extension Notification.Name {
 
 class SessionController: SessionControllable {
 
-    @ThreadSafe
-    private(set) var currentSession: EmbraceSession?
-
-    @ThreadSafe
-    private(set) var currentSessionSpan: Span?
-
     private let _attachmentCount = EmbraceAtomic<Int32>(0)
     internal var attachmentCount: Int { Int(_attachmentCount.load()) }
 
@@ -45,6 +39,16 @@ class SessionController: SessionControllable {
     private let uploader: SessionUploader
     weak var config: EmbraceConfig?
     weak var sdkStateProvider: EmbraceSDKStateProvider?
+
+    private var _currentSession: EmbraceSession?
+    var currentSession: EmbraceSession? {
+        lock.locked { _currentSession }
+    }
+
+    private var _currentSessionSpan: Span?
+    var currentSessionSpan: Span? {
+        lock.locked { _currentSessionSpan }
+    }
 
     private var backgroundSessionsEnabled: Bool {
         return config?.isBackgroundSessionEnabled == true
@@ -95,43 +99,44 @@ class SessionController: SessionControllable {
 
     @discardableResult
     func startSession(state: SessionState, startTime: Date = Date()) -> EmbraceSession? {
-        // end current session first
-        if currentSession != nil {
-            endSession()
-        }
-
-        guard sdkStateProvider?.isEnabled == true else {
-            return nil
-        }
-
-        guard let storage = storage else {
-            return nil
-        }
-
-        // detect cold start
-        let isColdStart = firstSession
-
-        // Don't start background session if the config is disabled.
-        //
-        // Note: There's an exception for the cold start session:
-        // We start the session anyways and we drop it when it ends if
-        // it's still considered a background session.
-        // Due to how iOS works we can't know for sure the state when the
-        // app starts, so we need to delay the logic!
-        //
-        // +
-        if isColdStart == false && state == .background && backgroundSessionsEnabled == false {
-            return nil
-        }
-        // -
 
         // we lock after end session to avoid a deadlock
         let session = lock.locked {
 
+            // end current session first
+            if _currentSession != nil {
+                endSessionNoLock()
+            }
+
+            guard sdkStateProvider?.isEnabled == true else {
+                return nil as EmbraceSession?
+            }
+
+            guard let storage = storage else {
+                return nil
+            }
+
+            // detect cold start
+            let isColdStart = firstSession
+
+            // Don't start background session if the config is disabled.
+            //
+            // Note: There's an exception for the cold start session:
+            // We start the session anyways and we drop it when it ends if
+            // it's still considered a background session.
+            // Due to how iOS works we can't know for sure the state when the
+            // app starts, so we need to delay the logic!
+            //
+            // +
+            if isColdStart == false && state == .background && backgroundSessionsEnabled == false {
+                return nil
+            }
+            // -
+
             // create session span
             let newId = SessionIdentifier.random
             let span = SessionSpanUtils.span(id: newId, startTime: startTime, state: state, coldStart: isColdStart)
-            currentSessionSpan = span
+            _currentSessionSpan = span
 
             // create session record and save it
             let session = storage.addSession(
@@ -143,7 +148,7 @@ class SessionController: SessionControllable {
                 startTime: startTime,
                 coldStart: isColdStart
             )
-            currentSession = session
+            _currentSession = session
 
             // start heartbeat
             heartbeat.start()
@@ -155,9 +160,83 @@ class SessionController: SessionControllable {
         }
 
         // post notification
-        NotificationCenter.default.post(name: .embraceSessionDidStart, object: session)
+        if let session {
+            NotificationCenter.default.post(name: .embraceSessionDidStart, object: session)
+        }
 
         return session
+    }
+
+    /// Ends the session session taking into account that the lock is held externally
+    @discardableResult
+    private func endSessionNoLock() -> Date {
+
+        guard let session = _currentSession else {
+            return Date()
+        }
+
+        // stop heartbeat
+        heartbeat.stop()
+        let now = Date()
+
+        guard sdkStateProvider?.isEnabled == true else {
+            deleteNoLock()
+            return now
+        }
+
+        // If the session is a background session and background sessions
+        // are disabled in the config, we drop the session!
+        // +
+        if session.coldStart == true && session.state == SessionState.background.rawValue
+            && backgroundSessionsEnabled == false
+        {
+            deleteNoLock()
+            return now
+        }
+        // -
+
+        // auto terminate spans
+        EmbraceOTel.processor?.autoTerminateSpans()
+
+        // post public notification
+        // WARNING: This is behind a lock, this is a problem, find a way to move this out of the lock.
+        NotificationCenter.default.post(name: .embraceSessionWillEnd, object: _currentSession)
+
+        // end log batches
+        logBatcher?.forceEndCurrentBatch(waitUntilFinished: true)
+
+        // end span
+        if let _currentSessionSpan {
+            // Ending span for otel processors
+            // Note: our exporter wont trigger an update on the stored span
+            // to prevent race conditions.
+            _currentSessionSpan.end(time: now)
+
+            // Manually updating the span record synchronously.
+            storage?.endSpan(
+                id: _currentSessionSpan.context.spanId.hexString,
+                traceId: _currentSessionSpan.context.traceId.hexString,
+                endTime: now
+            )
+        }
+
+        // update session end time and clean exit
+        if session.id != nil {
+            _currentSession = storage?.updateSession(session: session, endTime: now, cleanExit: true)
+        }
+
+        // post internal notification
+        if session.state == SessionState.foreground.rawValue {
+            Embrace.notificationCenter.post(name: .embraceForegroundSessionDidEnd, object: now)
+        }
+
+        // upload session
+        uploadSessionNoLock()
+
+        _currentSession = nil
+        _currentSessionSpan = nil
+
+        return now
     }
 
     /// Ends the session
@@ -165,118 +244,60 @@ class SessionController: SessionControllable {
     /// - Returns: The `endTime` of the session
     @discardableResult
     func endSession() -> Date {
-        guard let session = currentSession else {
-            return Date()
-        }
-
         return lock.locked {
-            // stop heartbeat
-            heartbeat.stop()
-            let now = Date()
-
-            guard sdkStateProvider?.isEnabled == true else {
-                delete()
-                return now
-            }
-
-            // If the session is a background session and background sessions
-            // are disabled in the config, we drop the session!
-            // +
-            if session.coldStart == true && session.state == SessionState.background.rawValue
-                && backgroundSessionsEnabled == false
-            {
-                delete()
-                return now
-            }
-            // -
-
-            // auto terminate spans
-            EmbraceOTel.processor?.autoTerminateSpans()
-
-            // post public notification
-            NotificationCenter.default.post(name: .embraceSessionWillEnd, object: currentSession)
-
-            // end log batches
-            logBatcher?.forceEndCurrentBatch(waitUntilFinished: true)
-
-            // end span
-            if let currentSessionSpan {
-                // Ending span for otel processors
-                // Note: our exporter wont trigger an update on the stored span
-                // to prevent race conditions.
-                currentSessionSpan.end(time: now)
-
-                // Manually updating the span record synchronously.
-                storage?.endSpan(
-                    id: currentSessionSpan.context.spanId.hexString,
-                    traceId: currentSessionSpan.context.traceId.hexString,
-                    endTime: now
-                )
-            }
-
-            // update session end time and clean exit
-            if session.id != nil {
-                currentSession = storage?.updateSession(session: session, endTime: now, cleanExit: true)
-            }
-
-            // post internal notification
-            if session.state == SessionState.foreground.rawValue {
-                Embrace.notificationCenter.post(name: .embraceForegroundSessionDidEnd, object: now)
-            }
-
-            // upload session
-            uploadSession()
-
-            currentSession = nil
-            currentSessionSpan = nil
-
-            return now
+            endSessionNoLock()
         }
     }
 
     func update(state: SessionState) {
-        guard let session = currentSession else {
-            return
-        }
+        lock.locked {
+            guard let session = _currentSession else {
+                return
+            }
 
-        currentSession = storage?.updateSession(session: session, state: state)
+            _currentSession = storage?.updateSession(session: session, state: state)
 
-        if let span = currentSessionSpan {
-            SessionSpanUtils.setState(span: span, state: state)
-            Embrace.client?.flush(span)
+            if let span = _currentSessionSpan {
+                SessionSpanUtils.setState(span: span, state: state)
+                Embrace.client?.flush(span)
+            }
         }
     }
 
     func update(appTerminated: Bool) {
-        guard let session = currentSession else {
-            return
-        }
+        lock.locked {
+            guard let session = _currentSession else {
+                return
+            }
 
-        currentSession = storage?.updateSession(session: session, appTerminated: appTerminated)
+            _currentSession = storage?.updateSession(session: session, appTerminated: appTerminated)
 
-        if let span = currentSessionSpan {
-            SessionSpanUtils.setTerminated(span: span, terminated: appTerminated)
-            Embrace.client?.flush(span)
+            if let span = _currentSessionSpan {
+                SessionSpanUtils.setTerminated(span: span, terminated: appTerminated)
+                Embrace.client?.flush(span)
+            }
         }
     }
 
     func update(heartbeat: Date) {
-        guard let session = currentSession else {
-            return
-        }
+        lock.locked {
+            guard let session = _currentSession else {
+                return
+            }
 
-        currentSession = storage?.updateSession(session: session, lastHeartbeatTime: heartbeat)
+            _currentSession = storage?.updateSession(session: session, lastHeartbeatTime: heartbeat)
 
-        if let span = currentSessionSpan {
-            SessionSpanUtils.setHeartbeat(span: span, heartbeat: heartbeat)
-            Embrace.client?.flush(span)
+            if let span = _currentSessionSpan {
+                SessionSpanUtils.setHeartbeat(span: span, heartbeat: heartbeat)
+                Embrace.client?.flush(span)
+            }
         }
     }
 
-    func uploadSession() {
+    func uploadSessionNoLock() {
         guard let storage = storage,
             let upload = upload,
-            let session = currentSession
+            let session = _currentSession
         else {
             return
         }
@@ -284,6 +305,10 @@ class SessionController: SessionControllable {
         queue.async { [weak self] in
             self?.uploader.uploadSession(session, storage: storage, upload: upload)
         }
+    }
+
+    func uploadSession() {
+        lock.locked { uploadSessionNoLock() }
     }
 
     func increaseAttachmentCount() {
@@ -297,7 +322,11 @@ extension SessionController {
     }
 
     private func delete() {
-        guard let session = currentSession else {
+        lock.locked { deleteNoLock() }
+    }
+
+    private func deleteNoLock() {
+        guard let session = _currentSession else {
             return
         }
 
@@ -305,8 +334,8 @@ extension SessionController {
             storage?.deleteSession(id: sessionId)
         }
 
-        currentSession = nil
-        currentSessionSpan = nil
+        _currentSession = nil
+        _currentSessionSpan = nil
     }
 }
 
