35f45b0a1d09749c102189d6d1325f4ac09a4cb2
Fernando Draghi <44760248+ferdraghi@users.noreply.github.com>
2025-10-28 14:51:02 -0300

    [EMBR-9300] tvOS Test app: hello world project (#418)

---
+++ Examples/Embrace-tvOS-TestApp/Embrace-tvOS-TestApp/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Embrace-tvOS-TestApp
+//
+//  Created by Fernando Draghi on 14/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button {
+                
+            } label: {
+                Text("Button 1")
+            }
+            
+            Button {
+                
+            } label: {
+                Text("Button 2")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
