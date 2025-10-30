7a95c348610f5592cd150cddf851a8e82ecee7ed
Fernando Draghi <ferna182@gmail.com>
2025-10-30 11:25:03 -0300

    tweaked login screen design

--- Examples/Embrace-tvOS-TestApp/Embrace-tvOS-TestApp/Embrace_tvOS_TestAppApp.swift
+++ Examples/Embrace-tvOS-TestApp/Embrace-tvOS-TestApp/Embrace_tvOS_TestAppApp.swift
@@ -2,15 +2,16 @@
 //  Embrace_tvOS_TestAppApp.swift
 //  Embrace-tvOS-TestApp
 //
-//  Created by Fernando Draghi on 14/10/2025.
 //
 
 import SwiftUI
 import EmbraceIO
+import EmbraceConfiguration
 
 @main
 struct Embrace_tvOS_TestAppApp: App {
     init() {
+        
         _ = try? Embrace.setup(options: .init(appId: "ejqby")).start()
     }
     var body: some Scene {
--- Examples/Embrace-tvOS-TestApp/Embrace-tvOS-TestApp/LoginScreen.swift
+++ Examples/Embrace-tvOS-TestApp/Embrace-tvOS-TestApp/LoginScreen.swift
@@ -2,41 +2,48 @@
 //  LoginScreen.swift
 //  Embrace-tvOS-TestApp
 //
-//  Created by Fernando Draghi on 14/10/2025.
 //
 
 import SwiftUI
 import EmbraceCore
+import EmbraceMacros
 
 struct LoginScreen: View {
     @State var username: String = ""
     @State var userId: String = ""
     @State var userEmail: String = ""
-
+    
     var body: some View {
-        VStack {
+        VStack(alignment: .center) {
             HStack {
-                Text("User")
-                    .frame(width: 500, alignment: .trailing)
-                TextField("UserName", text: $username)
-                    .submitLabel(.continue)
-                    .frame(width: 500)
-            }
-            HStack {
-                Text("User Email")
-                    .frame(width: 500, alignment: .trailing)
-                TextField("User email", text: $userEmail)
-                    .submitLabel(.continue)
-                    .frame(width: 500)
-            }
-            HStack {
-                Text("User Id")
-                    .frame(width: 500, alignment: .trailing)
-                TextField("User id", text: $userId)
-                    .submitLabel(.continue)
-                    .frame(width: 500)
+                Image(.embraceLogo)
+                    .renderingMode(.template)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100)
+                    .colorMultiply(.embraceYellow)
+                Text("embrace")
+                    .font(.embraceFont(size: 80))
+                    .foregroundStyle(.embraceYellow)
             }
             
+            Text("tvOS Test App")
+                .font(.embraceFont(size: 60))
+                .foregroundStyle(.embraceSteel)
+            
+            TextField("UserName", text: $username)
+                .font(.embraceFontLight(size: 30))
+                .submitLabel(.continue)
+                .frame(width: 500)
+            TextField("User email", text: $userEmail)
+                .font(.embraceFontLight(size: 30))
+                .submitLabel(.continue)
+                .frame(width: 500)
+            
+            TextField("User id", text: $userId)
+                .font(.embraceFontLight(size: 30))
+                .submitLabel(.continue)
+                .frame(width: 500)
             
             Button {
                 Embrace.client?.metadata.userName = username
@@ -44,7 +51,9 @@ struct LoginScreen: View {
                 Embrace.client?.metadata.userEmail = userEmail
             } label: {
                 Text("\"Login\"")
-            }
+                    .font(.embraceFont(size: 30))
+                    .foregroundStyle(.embraceSilver)
+            }.accessibilityLabel("Login Button")
             
             Button {
                 Embrace.client?.metadata.userName = nil
@@ -52,9 +61,12 @@ struct LoginScreen: View {
                 Embrace.client?.metadata.userEmail = nil
             } label: {
                 Text("Skip")
-            }
+                    .font(.embraceFont(size: 30))
+                    .foregroundStyle(.embraceSilver)
+            }.accessibilityLabel("Skip Login Button")
         }
         .padding()
+        .embraceTrace("Login View")
     }
 }
 
