ad4dc922e5501dc34327625296ecab088a876b06
Fernando Draghi <ferna182@gmail.com>
2025-11-21 10:25:31 -0300

    added helper functions to view model to return years with available media for streaming.

--- Examples/Embrace-tvOS-TestApp/Embrace-tvOS-TestApp/Screens/Channels/ChannelsScreenViewModel.swift
+++ Examples/Embrace-tvOS-TestApp/Embrace-tvOS-TestApp/Screens/Channels/ChannelsScreenViewModel.swift
@@ -16,7 +16,7 @@ class ChannelsScreenViewModel: ObservableObject {
     }
     
     @Published private(set) var status: Status = .notStarted
-    
+    private var wwdcData: WWDCData? = nil
     private let fetchController: FetchController
     
     init(fetchController: FetchController) {
@@ -31,10 +31,36 @@ class ChannelsScreenViewModel: ObservableObject {
         status = .fetching
         
         do {
-            let wwdcdata = try await fetchController.fetchWWDCData()
+            wwdcData = try await fetchController.fetchWWDCData()
             status = .success
         } catch {
             status = .failed(error: error)
+        }
+    }
+    
+    func sessionsFor(year: Int) -> [WWDCSession]? {
+        guard let wwdcData = wwdcData else { return nil }
+        
+        let filtered = wwdcData.sessions.filter {
+            $0.year == year
+        }
+        
+        let onlyWithAvailableMedia = filtered.filter {
+            $0.media?.containsStreamingMedia ?? false
+        }
+        
+        return onlyWithAvailableMedia.sorted { $0.eventContentId < $1.eventContentId }
+    }
+    
+    var availableYears: [Int] {
+        let years = Set<Int>(wwdcData?.sessions.map(\.year) ?? [])
+        
+        return years.sorted()
+    }
+    
+    var yearsWithAvailableMedia: [Int] {
+        availableYears.filter {
+            sessionsFor(year: $0)?.count ?? 0 > 0
         }
     }
 }
