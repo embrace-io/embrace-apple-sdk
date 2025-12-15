982d66c868b8bc98967824d324790250a60073b5
Márton Marczell <mmarczell@graphisoft.com>
2025-12-15 14:30:21 +0100

    Add standard OTel property os.name to logs

--- Tests/EmbraceCoreTests/Capture/OneTimeServices/DeviceInfoCaptureServiceTests.swift
+++ Tests/EmbraceCoreTests/Capture/OneTimeServices/DeviceInfoCaptureServiceTests.swift
@@ -27,7 +27,7 @@ final class DeviceInfoCaptureServiceTests: XCTestCase {
         let processId = ProcessIdentifier.current.stringValue
 
         let resources = handler.fetchResourcesForProcessId(ProcessIdentifier.current)
-        XCTAssertEqual(resources.count, 10)
+        XCTAssertEqual(resources.count, 11)
 
         // jailbroken
         let jailbroken = handler.fetchMetadata(
@@ -98,6 +98,15 @@ final class DeviceInfoCaptureServiceTests: XCTestCase {
         )
         XCTAssertNotNil(osVariant)
         XCTAssertEqual(osVariant!.value, EMBDevice.operatingSystemType)
+		
+		let osName = handler.fetchMetadata(
+			key: SemanticConventions.Os.name.rawValue,
+			type: .requiredResource,
+			lifespan: .process,
+			lifespanId: processId
+		)
+		XCTAssertNotNil(osName)
+		XCTAssertEqual(osName!.value, EMBDevice.operatingSystemType)
 
         // model
         let model = handler.fetchMetadata(
