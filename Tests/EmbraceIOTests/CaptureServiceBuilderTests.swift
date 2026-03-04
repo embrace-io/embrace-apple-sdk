da180d9f728a55264a7afe05ea98fbe96d8fdf73
Ignacio Tischelman <ignacio.tischelman@embrace.io>
2026-03-04 12:44:19 -0300

    Adding new queue for upload retry process

--- Tests/EmbraceIOTests/CaptureServiceBuilderTests.swift
+++ Tests/EmbraceIOTests/CaptureServiceBuilderTests.swift
@@ -57,7 +57,7 @@ class CaptureServiceBuilderTests: XCTestCase {
         // then the list contains the correct services
         let list = builder.build()
 
-        var count = 4
+        var count = 3
 
         #if canImport(UIKit) && !os(watchOS)
             count += 2
@@ -73,7 +73,7 @@ class CaptureServiceBuilderTests: XCTestCase {
         XCTAssertNotNil(list.first(where: { $0 is LowPowerModeCaptureService }))
         XCTAssertNotNil(list.first(where: { $0 is LowMemoryWarningCaptureService }))
         XCTAssertNotNil(list.first(where: { $0 is LowPowerModeCaptureService }))
-        XCTAssertNotNil(list.first(where: { $0 is HangCaptureService }))
+        XCTAssertNil(list.first(where: { $0 is HangCaptureService }))
 
         let service = list.first(where: { $0 is URLSessionCaptureService }) as! URLSessionCaptureService
         XCTAssertFalse(service.options.injectTracingHeader)
@@ -100,7 +100,7 @@ class CaptureServiceBuilderTests: XCTestCase {
         // then the list contains the correct services
         let list = builder.build()
 
-        var count = 3
+        var count = 2
 
         #if canImport(WebKit)
             count += 1
@@ -108,7 +108,7 @@ class CaptureServiceBuilderTests: XCTestCase {
         #endif
         XCTAssertNotNil(list.first(where: { $0 is LowMemoryWarningCaptureService }))
         XCTAssertNotNil(list.first(where: { $0 is LowPowerModeCaptureService }))
-        XCTAssertNotNil(list.first(where: { $0 is HangCaptureService }))
+        XCTAssertNil(list.first(where: { $0 is HangCaptureService }))
 
         XCTAssertEqual(list.count, count)
     }
