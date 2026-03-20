ef95581edf6347ab7d33c3f35446c9fd756ebf4b
Fernando Draghi <44760248+ferdraghi@users.noreply.github.com>
2026-03-20 18:42:30 -0300

    [EMBR-11277] Fix: SDK not compiling for Watchos (#547)

--- Sources/EmbraceIO/Options/EmbraceIO+CaptureServicesOptions.swift
+++ Sources/EmbraceIO/Options/EmbraceIO+CaptureServicesOptions.swift
@@ -15,9 +15,15 @@ extension EmbraceIO {
     /// Refer to `CaptureServicesOptionsBuilder` if you want a custom setup.
     public final class CaptureServicesOptions {
         let urlSession: URLSessionCaptureService.Options?
-        let tap: TapCaptureService.Options?
-        let view: ViewCaptureService.Options?
-        let webView: WebViewCaptureService.Options?
+        #if !os(watchOS)
+            #if canImport(UIKit)
+                let tap: TapCaptureService.Options?
+                let view: ViewCaptureService.Options?
+            #endif
+            #if canImport(WebKit)
+                let webView: WebViewCaptureService.Options?
+            #endif
+        #endif
         let pushNotification: PushNotificationCaptureService.Options?
         let lowMemoryWarning: Bool
         let lowPowerMode: Bool
@@ -27,28 +33,89 @@ extension EmbraceIO {
         public class func `default`() -> EmbraceIO.CaptureServicesOptions {
             return CaptureServicesOptions()
         }
+        #if os(watchOS)
+            internal init(
+                urlSession: URLSessionCaptureService.Options? = .init(),
+                pushNotification: PushNotificationCaptureService.Options? = nil,
+                lowMemoryWarning: Bool = true,
+                lowPowerMode: Bool = true,
+                hang: Bool = false,
+                customServices: [CaptureService] = []
+            ) {
+                self.urlSession = urlSession
+                self.pushNotification = pushNotification
+                self.lowMemoryWarning = lowMemoryWarning
+                self.lowPowerMode = lowPowerMode
+                self.hang = hang
+                self.customServices = customServices
+            }
+        #else
+            #if canImport(UIKit)
+                #if canImport(WebKit)
+                    internal init(
+                        urlSession: URLSessionCaptureService.Options? = .init(),
+                        tap: TapCaptureService.Options? = .init(),
+                        view: ViewCaptureService.Options? = .init(),
+                        webView: WebViewCaptureService.Options? = .init(),
+                        pushNotification: PushNotificationCaptureService.Options? = nil,
+                        lowMemoryWarning: Bool = true,
+                        lowPowerMode: Bool = true,
+                        hang: Bool = false,
+                        customServices: [CaptureService] = []
+                    ) {
+                        self.urlSession = urlSession
+                        self.tap = tap
+                        self.view = view
+                        self.webView = webView
+                        self.pushNotification = pushNotification
+                        self.lowMemoryWarning = lowMemoryWarning
+                        self.lowPowerMode = lowPowerMode
+                        self.hang = hang
+                        self.customServices = customServices
+                    }
+                #else
+                    internal init(
+                        urlSession: URLSessionCaptureService.Options? = .init(),
+                        tap: TapCaptureService.Options? = .init(),
+                        view: ViewCaptureService.Options? = .init(),
+                        pushNotification: PushNotificationCaptureService.Options? = nil,
+                        lowMemoryWarning: Bool = true,
+                        lowPowerMode: Bool = true,
+                        hang: Bool = false,
+                        customServices: [CaptureService] = []
+                    ) {
+                        self.urlSession = urlSession
+                        self.tap = tap
+                        self.view = view
+                        self.pushNotification = pushNotification
+                        self.lowMemoryWarning = lowMemoryWarning
+                        self.lowPowerMode = lowPowerMode
+                        self.hang = hang
+                        self.customServices = customServices
+                    }
+                #endif
+            #endif
+            #if !canImport(UIKit) && canImport(WebKit)
+                internal init(
+                    urlSession: URLSessionCaptureService.Options? = .init(),
+                    webView: WebViewCaptureService.Options? = .init(),
+                    pushNotification: PushNotificationCaptureService.Options? = nil,
+                    lowMemoryWarning: Bool = true,
+                    lowPowerMode: Bool = true,
+                    hang: Bool = false,
+                    customServices: [CaptureService] = []
+                ) {
+                    self.urlSession = urlSession
+                    self.webView = webView
+                    self.pushNotification = pushNotification
+                    self.lowMemoryWarning = lowMemoryWarning
+                    self.lowPowerMode = lowPowerMode
+                    self.hang = hang
+                    self.customServices = customServices
+                }
+            #endif
 
-        internal init(
-            urlSession: URLSessionCaptureService.Options? = .init(),
-            tap: TapCaptureService.Options? = .init(),
-            view: ViewCaptureService.Options? = .init(),
-            webView: WebViewCaptureService.Options? = .init(),
-            pushNotification: PushNotificationCaptureService.Options? = nil,
-            lowMemoryWarning: Bool = true,
-            lowPowerMode: Bool = true,
-            hang: Bool = false,
-            customServices: [CaptureService] = []
-        ) {
-            self.urlSession = urlSession
-            self.tap = tap
-            self.view = view
-            self.webView = webView
-            self.pushNotification = pushNotification
-            self.lowMemoryWarning = lowMemoryWarning
-            self.lowPowerMode = lowPowerMode
-            self.hang = hang
-            self.customServices = customServices
-        }
+        #endif
 
         var list: [CaptureService] {
             var services: [CaptureService] = []
@@ -57,20 +124,23 @@ extension EmbraceIO {
             if let urlSessionOptions = urlSession {
                 services.append(URLSessionCaptureService(options: urlSessionOptions))
             }
+            #if !os(watchOS)
+                #if canImport(UIKit)
+                    // tap
+                    if let tapOptions = tap {
+                        services.append(TapCaptureService(options: tapOptions))
+                    }
 
-            // tap
-            if let tapOptions = tap {
-                services.append(TapCaptureService(options: tapOptions))
-            }
-
-            if let viewOptions = view {
-                services.append(ViewCaptureService(options: viewOptions))
-            }
-
-            if let webViewOptions = webView {
-                services.append(WebViewCaptureService(options: webViewOptions))
-            }
-
+                    if let viewOptions = view {
+                        services.append(ViewCaptureService(options: viewOptions))
+                    }
+                #endif
+                #if canImport(WebKit)
+                    if let webViewOptions = webView {
+                        services.append(WebViewCaptureService(options: webViewOptions))
+                    }
+                #endif
+            #endif
             if let pushNotificationOptions = pushNotification {
                 services.append(PushNotificationCaptureService(options: pushNotificationOptions))
             }
