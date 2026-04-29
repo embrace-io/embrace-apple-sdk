264896eebe98f36f952f11900111c0af28068d81
Karl Stenerud <kstenerud@gmail.com>
2026-04-29 18:20:43 +0200

    Review fixes: copyright 2026, ProfilingSample/ProfilingResult conformances

--- Sources/EmbraceProfilingSampler/include/emb_sampler.h
+++ Sources/EmbraceProfilingSampler/include/emb_sampler.h
@@ -1,5 +1,5 @@
 //
-//  Copyright © 2024 Embrace Mobile, Inc. All rights reserved.
+//  Copyright © 2026 Embrace Mobile, Inc. All rights reserved.
 //
 
 /// Lock-free profiling sampler that periodically captures stack traces of the
